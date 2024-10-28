7dbc27b2f658d862624d4f75fd272c7b29a033ea
Kali <muppal03@email.franklin.edu>
2024-10-28 15:03:04 -0400

    test

--- src/vendorDashboard/data/apiPath.jsx
+++ src/vendorDashboard/data/apiPath.jsx
@@ -1,2 +1,2 @@
-// export let API_Path="http://localhost:3000"
-export let API_Path="https://online-restaurant-prbp.onrender.com/"
+export let API_Path="http://localhost:3000"
+// export let API_Path="https://online-restaurant-prbp.onrender.com/"
